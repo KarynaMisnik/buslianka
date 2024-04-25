665d8ff01ff7a7d434a5bd8c5255e77a9763ec3d
KarynaMisnik <karinamisnik94@gmail.com>
2024-04-25 11:00:17 +0300

    footer translation btns added
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

---
+++ main.js
@@ -0,0 +1,9 @@
+function loadEnglish() {
+  // Redirect to the English version (index.html)
+  window.location.href = "/index.html";
+}
+
+function loadBelarusian() {
+  // Redirect to the Belarusian version (index-by.html)
+  window.location.href = "index-by.html";
+}
