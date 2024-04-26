4c2550ed9debbb2973e08e5d443aaa0f78fc0da1
KarynaMisnik <karinamisnik94@gmail.com>
2024-04-26 15:14:23 +0300

    main.js updated, tribute page upd
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ function loadBelarusian() {
   // Redirect to the Belarusian version (index-by.html)
   window.location.href = "index-by.html";
 }
+
+/* Manipulates img logo in pages: replaces one image with another one on hover */
+const img = document.getElementById("home-img");
+
+img.addEventListener("mouseenter", function () {
+  // Change the image source on hover
+  this.src = "../../img/stork.png";
+});
+
+img.addEventListener("mouseleave", function () {
+  // Change back to the original image source on mouse leave
+  this.src = "../../img/nest.png";
+});
